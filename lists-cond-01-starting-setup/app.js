04d6188c6899eead1d3b2f2a3da4318a80ec24c1
BerkiManman <berkayguler7@hotmail.com>
2023-07-06 20:37:24 +0300

    conditionals and loops

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,7 +1,19 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      goalValue: '',
+      goals: [],
+    };
   },
+  methods: {
+    addGoal() {
+      this.goals.push(this.goalValue);
+      console.log(this.goals);
+    },
+    deleteGoal(index) {
+      this.goals.splice(index, 1);
+    },
+  }
 });
 
 app.mount('#user-goals');
