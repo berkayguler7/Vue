9f96c108658b2a637282e794274dc06378a7c010
BerkiManman <berkayguler7@hotmail.com>
2023-07-08 17:15:11 +0300

    put app2 into template

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -18,6 +18,9 @@ const app = Vue.createApp({
 app.mount('#app');
 
 const app2 = Vue.createApp({
+  template: `
+  <h2>How Vue Works2</h2>
+  <p>{{ favoriteMeal }}</p>`,
   data() {
     return {
       favoriteMeal: 'Pizza',
