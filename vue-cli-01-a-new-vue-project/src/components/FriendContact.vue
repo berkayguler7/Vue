8d8182dcb631e2e85e96807f761f2680e50675e8
BerkiManman <berkayguler7@hotmail.com>
2023-07-08 21:36:28 +0300

    added favorite functionality without changing prop

--- vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,7 +1,8 @@
 <template>
     <li>
-        <h2>{{ friend.name }}</h2>
+        <h2>{{ friend.name }} {{ this.isFavorite ? '(Favorite)' : '' }}</h2>
         <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <button @click="toggleFavorite">{{ friend.isFavorite ? 'Unfavorite' : 'Favorite' }}</button>
         <ul v-if="detailsAreVisible">
             <li><strong>Phone:</strong> {{ friend.phone }}</li>
             <li><strong>Email:</strong> {{ friend.email }}</li>
@@ -20,12 +21,16 @@ export default {
     data() {
         return {
             detailsAreVisible: false,
+            isFavorite: false,
         };
     },
     methods: {
         toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
-        }
+        },
+        toggleFavorite() {
+            this.isFavorite = !this.isFavorite;
+        },
     },
 }
 </script>
