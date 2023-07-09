c43e327fda01e28d47e73feeb6b4b4486a1a715f
BerkiManman <berkayguler7@hotmail.com>
2023-07-09 12:49:45 +0300

    communication assignment complete

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<h1>Communication Assignment</h1>
+	<div>
+		<active-user
+			@change-user="changeUser"
+			:name="name"
+			:age="age"
+		></active-user>
+		<user-data @submit-user="submitUser"></user-data>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			name: "Max",
+			age: 31,
+		};
+	},
+	methods: {
+		submitUser(name, age) {
+			this.name = name;
+			this.age = age;
+		},
+		changeUser(name, age) {
+			this.name = name;
+			this.age = age;
+		},
+	},
+};
+</script>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,28 @@
+<template>
+	<form @submit.prevent="submitUser">
+		<label>Username</label>
+		<input type="text" placeholder="Username" v-model="username" />
+		<br />
+		<label>Age</label>
+		<input type="number" placeholder="Age" v-model="age" />
+		<br /><br />
+		<button>Submit</button>
+	</form>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			username: "",
+			age: "",
+		};
+	},
+	emits: ["submit-user"],
+	methods: {
+		submitUser() {
+			this.$emit("submit-user", this.username, this.age);
+		},
+	},
+};
+</script>
