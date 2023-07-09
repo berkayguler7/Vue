dd18224ea0862ed05f2049e5178ef8a978aef233
BerkiManman <berkayguler7@hotmail.com>
2023-07-09 11:55:26 +0300

    feat: add friends

---
+++ vue-cli-01-a-new-vue-project/src/components/NewFriend.vue
@@ -0,0 +1,37 @@
+<template>
+    <form @submit.prevent="submitData">
+        <label>Your Name</label>
+        <input type="text" placeholder="Name" v-model="name" />
+        <br>
+        <label>Your E-Mail</label>
+        <input type="email" placeholder="E-Mail" v-model="email" />
+        <br>
+        <label>Your Phone</label>
+        <input type="tel" placeholder="Phone" v-model="phone" />
+        <br><br>
+        <button>Add Friend</button>
+    </form>
+</template>
+
+<script>
+export default {
+    emits: ['add-friend'],
+    data() {
+        return {
+            name: '',
+            email: '',
+            phone: ''
+        }
+    },
+    methods: {
+        submitData() {
+            this.$emit('add-friend', {
+                id: new Date().toISOString(),
+                name: this.name,
+                email: this.email,
+                phone: this.phone
+            });
+        }
+    }
+};
+</script>
